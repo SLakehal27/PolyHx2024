a007a3e7fcd188b49443987d2bcbb28a6ee0b5e0
Selim Lakehal <113135101+SLakehal27@users.noreply.github.com>
2024-06-27 10:06:10 -0400

    Update env.ts

--- client/src/app/env/env.ts
+++ client/src/app/env/env.ts
@@ -2,13 +2,13 @@ export const environment = {
     production: false,
     openAiApiKey: 'insert_key_here',
     firebase : {
-        apiKey: "AIzaSyBI9Fs5Yhjlj97EcBthi_leSE7kEO0e7Zs",
-        authDomain: "poly-hacks-cf820.firebaseapp.com",
-        projectId: "poly-hacks-cf820",
-        storageBucket: "poly-hacks-cf820.appspot.com",
-        messagingSenderId: "260682960041",
-        appId: "1:260682960041:web:c01f7c0aac892e3597adb8",
-        measurementId: "G-CSM12JZ99R"
+        apiKey: "insert_key_here",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
       }
   };
   
